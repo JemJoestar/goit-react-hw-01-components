093aa8f9b214256c7688dec975be57089ff746f0
JemJoestar <jemapro2@gmail.com>
2023-08-25 20:35:34 +0300

    try to fix

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { TransactionTable, Transactions } from './TransactionHistory.styled';
 
-export const TransactionHistory = ({items}) => {
+export const TransactionHistory = ({ items }) => {
   return (
     <TransactionTable>
       <thead>
@@ -12,16 +12,27 @@ export const TransactionHistory = ({items}) => {
         </tr>
       </thead>
       <Transactions>
-        {items.map(({id, type, amount, currency }) => {
-            return  <tr key={id}>
-            <td>{type}</td>
-            <td>{amount}</td>
-            <td>{currency}</td>
-          </tr>
+        {items.map(({ id, type, amount, currency }) => {
+          return (
+            <tr key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
         })}
       </Transactions>
     </TransactionTable>
   );
 };
 
-
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
